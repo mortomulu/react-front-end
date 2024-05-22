ad72fa351f2b32170f55ab176f5027e86e954838
HenryFtNonna <118751951+HenryFtNonna@users.noreply.github.com>
2024-05-22 11:56:05 +0700

    initialization Project

---
+++ capstone-react-9/src/pages/Home/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroSection from '../../components/HeroSection/HeroSection'
+import Footer from '../../components/Footer/Footer'
+
+
+export default function HomePage() {
+  return (
+    <div>HomePage
+        <HeroSection/>
+        <Footer/>
+    </div>
+  )
+}
