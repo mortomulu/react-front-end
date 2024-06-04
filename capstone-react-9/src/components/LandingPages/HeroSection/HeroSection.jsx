c9723310ae432add0b31a49760910d6b0833a63e
NazarYosa <nazaryosa@gmail.com>
2024-06-05 00:02:12 +0700

    update Hero Section

--- capstone-react-9/src/components/LandingPages/HeroSection/HeroSection.jsx
+++ capstone-react-9/src/components/LandingPages/HeroSection/HeroSection.jsx
@@ -17,7 +17,7 @@ export default function HeroSection() {
         </p>
         <button
           type="button"
-          class="text-white bg-[#0075EB] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 text-[18px]"
+          className="text-white bg-[#0075EB] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 text-[18px]"
         >
           Download Sekarang!
         </button>
