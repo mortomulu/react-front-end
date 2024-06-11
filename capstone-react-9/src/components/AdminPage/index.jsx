cd896a9afe0d1e478d5b7d03c3d80a95817bafc7
NazarYosa <nazaryosa@gmail.com>
2024-06-11 22:26:20 +0700

    update sidebar, di routing

--- capstone-react-9/src/components/AdminPage/index.jsx
+++ capstone-react-9/src/components/AdminPage/index.jsx
@@ -4,7 +4,8 @@ import TransaksiPage from "./TransaksiPage/index.jsx"
 export default function adminPage() {
   return (
     <div className='flex font-Poppins' style={{ backgroundColor: '#E6F6FF' }}>
-        <DashboardPage/>
+      {/* <DashboardPage /> */}
+      <TransaksiPage />
     </div>
   )
 }
