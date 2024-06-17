fee5f348902ed247a27a269777c397252fe1d873
mortomulu <mfajrihidayat09@gmail.com>
2024-06-17 12:06:06 +0700

    feat: logic logout

--- capstone-react-9/src/service/accessCookie.js
+++ capstone-react-9/src/service/accessCookie.js
@@ -3,3 +3,7 @@ export const getCookie = (name) => {
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(';').shift();
 };
+
+export const deleteCookie = (name) => {
+    document.cookie = `${name}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC; SameSite=Strict`;
+};
